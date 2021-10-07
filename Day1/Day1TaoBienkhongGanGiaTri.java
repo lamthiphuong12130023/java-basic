da5085af8799f2170d6304ff9928c6b0434830f3
PhuongLam <lamphuong20994@gmail.com>
2021-10-07 19:32:22 +0700

    day1

---
+++ Day1/Day1TaoBienkhongGanGiaTri.java
@@ -0,0 +1,13 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class Day1TaoBienkhongGanGiaTri {
+    public static void main (String [] args){
+   int a;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Nhập a = ");
+        a=sc.nextInt();
+        System.out.println("A = "+a);
+    }
+}
